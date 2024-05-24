d1eb90a941f32fbd936685b1ce3a8b9f841823c1
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2024-05-24 00:11:10 -0700

    move comments

--- quirky/quirky.go
+++ quirky/quirky.go
@@ -17,3 +17,14 @@ func ClosetToPenguinSpeed(races race.Races) race.Race {
 
 	return clostRace
 }
+
+/*
+	- closest to running pace of a penguin (1.6 miles an hour).
+	- most improved
+	- most races
+	- closest time on the water to a soft boiled egg (4 min)
+	- closest race time to attention span of a feral pig
+	- money bags award, if time was money, this racer would be the richest (check first)
+	- How long does it take to pee 1.5 liters? gold painted toilet seat, and a container of apple juice. Urine Luck.
+	-
+*/
